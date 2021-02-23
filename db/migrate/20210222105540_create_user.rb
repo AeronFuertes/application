a488a9662b334361640e0a38e889790f2a634d46
Test App <fuertesdarrel@gmail.com>
2021-02-23 18:25:05 +0800

    create users table and model

--- db/migrate/20210222105540_create_user.rb
+++ db/migrate/20210222105540_create_user.rb
@@ -1,7 +1,7 @@
 class CreateUser < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :username
       t.string :email
 
       t.timestamps
