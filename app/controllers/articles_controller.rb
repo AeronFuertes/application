abfb4c906c2c19ebade419545e5cc41c7d05ed20
Test App <fuertesdarrel@gmail.com>
2021-02-24 16:55:33 +0800

    Add edit update and partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,6 +30,9 @@ class ArticlesController < ApplicationController
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
+
+    else
+      render 'edit'
     end
   end
 
