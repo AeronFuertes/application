2ba1eac4a590606b16509628cd017809d6a22729
Test App <fuertesdarrel@gmail.com>
2021-02-25 17:19:45 +0800

    update all my pages and put some link restrictions for login state and logout state

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -1,5 +1,6 @@
 class PlaceController < ApplicationController
   def home
+    redirect_to articles_path if logged_in?
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :show, :update]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -20,11 +20,11 @@ class UsersController < ApplicationController
 
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
+
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -43,6 +43,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
