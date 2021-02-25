c7fa12aba1d8eb98407c68cfb6f71ee11917e24c
Test App <fuertesdarrel@gmail.com>
2021-02-25 18:30:37 +0800

    Complete authentication system and update current_user to be article creator

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :show, :update]
+  before_action :require_same_user, only: [:edit, :update]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -44,8 +45,17 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
+
+
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "Hey you can't do that, mind your own user"
+      redirect_to root_path
+    end
   end
 
 end
